3938462e86d75715764ae51568dcd207bf9b178c
Christer Lennell <karlschrister@hotmail.com>
2026-02-12 12:19:31 +0100

    add

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css'
 import TodoList from './TodoList'
 
 function App() {
+
+  const [completedTodos, setComletedTodos] = useState([
+    { text: 'Take out the garbage', isCompleted: true },
+    { text: 'Make dinner', isCompleted: true }
+  ]);
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    { text: 'Paint the house', isCompleted: false }
+  ]);
+
   return (
     <>
-    <TodoList completedTodos={[]} incompleteTodos={[]}/>
+      <TodoList completedTodos={completedTodos} incompleteTodos={incompleteTodos} />
     </>
   );
 }
